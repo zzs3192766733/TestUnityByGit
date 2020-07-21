4c1e6fb8fc141787f6de2fef61aa1d5931b59433
zzs3192766733 <3192766733@qq.com>
2020-07-21 13:04:00 +0800

    3333222z

--- Assets/wcnm/A_4.cs
+++ Assets/wcnm/A_4.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class A-1 : MonoBehaviour
+public class A_4 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
