8352ece4ad39e74200562af5b310cd407a1e1170
zzs3192766733 <3192766733@qq.com>
2020-07-21 13:44:00 +0800

    3333222zs

---
+++ Assets/Zzs.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zzs : MonoBehaviour
+{
+    public void FindSbWy(GameObject gameObject)
+    {
+        gameObject.transform.position = this.transform.position;
+    }
+}
