5719629c4a67d8ecbcc6d005ed38e87696469866
wyzzspnblzq <2875844866@qq.com>
2020-07-21 13:56:50 +0800

    sdfsd

--- Assets/Zzs.cs
+++ Assets/Zzs.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Zzs : MonoBehaviour
 {
+    public static int wcnm;
     public void FindSbWy()
     {
         this.transform.position = this.transform.position + new Vector3(1, 1, 1);
@@ -11,5 +12,9 @@ public class Zzs : MonoBehaviour
     public void Zzsssb(string str)
     {
         Debug.Log("wcnm");
+    }
+    public static void Wcnm(int wcnm)
+    {
+        Zzs.wcnm = wcnm;
     }
 }
